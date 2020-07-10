714da3c7efa5637c9a5cba04759870837782a171
adarshashok7 <68113832+adarshashok7@users.noreply.github.com>
2020-07-10 18:47:17 +0530

    Add files via upload

---
+++ area.py
@@ -0,0 +1,4 @@
+r = float(input("Radius of The Circle: "))
+ar = 3.14*r*r
+print("The Area of the circle wirh radius %0.2f is : " %r)
+print("%f" %ar)
---
+++ fn.py
@@ -0,0 +1,3 @@
+filename = input("Input the filename: ")
+f = filename.split(".")
+print("The Extention is : '%s'" %f[-1])
